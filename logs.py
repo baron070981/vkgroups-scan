a0f5e6544c964253e1d22ea25aa895cf3bd9c2d4
baron070981 <baron070981@gmail.com>
2020-01-19 22:05:39 +0300

    исправлен logs.py

--- logs.py
+++ logs.py
@@ -1,7 +1,8 @@
 import os
 import sys
 import time
-
+from random import randint
+from pprint import pprint
 
 
 
@@ -14,19 +15,29 @@ class Logs:
             pass
     
     
-    def accum_logs(self, log_var):
-        log_str = str(log_var).strip()
-        t = time.ctime()
-        log = str(self.COUNT)+'. '+t+' :: '+log_str+'\n'
-        self.COUNT += 1
-        self.log_list.append(log)
+    def accum_logs(self, *log_var):
+        #print('Type:', type(log_var), ' Len:', len(log_var))
+        log_var = log_var[0]
+        if type(log_var) == list:
+            for data in log_var:
+                print('\nTo accum logs:', data, '\n'+'='*30)
+                t = time.ctime()
+                log = str(self.COUNT)+'. '+t+' :: '+str(data)
+                self.log_list.append(log)
+                self.COUNT += 1
+        else:
+            t = time.ctime()
+            log = str(self.COUNT)+'. '+t+' :: '+str(log_var)
+            self.log_list.append(log)
+            print('\nTo accum logs:', log_var, '\n'+'='*30)
+            self.COUNT +=1
     
     
     def write_from_loglist(self):
         if len(self.log_list) > 0:
             with open(self.filename, 'a') as f:
                 for data in self.log_list:
-                    f.write(data)
+                    f.write(data+'\n')
             self.log_list.clear()
     
     
@@ -42,17 +53,29 @@ class Logs:
 
 
 
-
-
-
-
-
-
-
 if __name__ == '__main__':
     l = Logs()
     for i in range(5):
         time.sleep(.700)
         l.accum_logs('error test')
+    
+    sl = list()
+    for i in range(5):
+        time.sleep(.700)
+        nums = [chr(randint(53, 77)) for i in range(randint(10,30))]
+        s = ''.join(nums)
+        sl.append(s)
+    
+    l.accum_logs(sl)
+    
     l.write_from_loglist()
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
